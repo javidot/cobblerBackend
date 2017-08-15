faad9520fbeac52ae5451d717da87f80ff02e706
Javier Cintron <massiel.javier@gmail.com>
2017-08-15 10:34:19 -0400

    First commit

---
+++ credentials_env.js
@@ -0,0 +1,6 @@
+module.exports = {
+    host     : 'cobberdevdbinstance.cnc53j0i9ssi.us-west-2.rds.amazonaws.com',
+    user     : 'cobblerdbuser',
+    password : '123abc$%^',
+    database : 'cobberdevdb'
+}
