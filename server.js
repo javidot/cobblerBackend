7c49898082067728a113147c6353baa4699dbac5
Javier Cintron <massiel.javier@gmail.com>
2017-08-15 20:57:47 -0400

    Changes to the api

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var http = require('http');
 
 var app = require('./app');
 
-var port = process.env.PORT || 80; 	  // set our port
+var port = process.env.PORT || 3000; 	  // set our port
 var host = process.env.HOST || '0.0.0.0'; // For Heroku to run successfully
 
 http.createServer(app).listen(port, host, () => {
