90ca3614c7eb4eac9a3283898cd7bc9f9d14e14a
Javier Cintron <massiel.javier@gmail.com>
2017-08-23 00:52:35 -0400

    Cors

--- routers/api.js
+++ routers/api.js
@@ -15,8 +15,9 @@ module.exports = (express, connection) => {
 	    console.log("You have hit the /api", req.method, req.url);
 
 	    // CORS 
-	    res.header("Access-Control-Allow-Origin", "*");
-	    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	    res.header('Access-Control-Allow-Origin', '*');
+	    res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE');
+	    res.header('Access-Control-Allow-Headers', '*');
 
 	    next();
 	});
