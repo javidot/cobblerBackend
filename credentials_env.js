0ca7878871b26c99a611ac61612fc311f62dd674
Javier Cintron <massiel.javier@gmail.com>
2017-08-23 13:29:02 -0400

    CORS

--- credentials_env.js
+++ credentials_env.js
@@ -1,14 +1,14 @@
-module.exports = {
-  host     : process.env.RDS_HOSTNAME,
-  user     : process.env.RDS_USERNAME,
-  password : process.env.RDS_PASSWORD,
-  port     : process.env.RDS_PORT
-}
-
 // module.exports = {
-//    host     : '127.0.0.1',
-//    user     : 'root',
-//    password : '123abc!@#',
-//    port     : '3306',
-//    database : 'ebdb'
+//   host     : process.env.RDS_HOSTNAME,
+//   user     : process.env.RDS_USERNAME,
+//   password : process.env.RDS_PASSWORD,
+//   port     : process.env.RDS_PORT
 // }
+
+module.exports = {
+   host     : '127.0.0.1',
+   user     : 'root',
+   password : '123abc!@#',
+   port     : '3306',
+   database : 'ebdb'
+}
