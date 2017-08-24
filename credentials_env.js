7f8f64be96cac90ab884f93adcc8fc4354639c3a
Javier Cintron <massiel.javier@gmail.com>
2017-08-24 08:59:39 -0400

    db host change

--- credentials_env.js
+++ credentials_env.js
@@ -14,7 +14,7 @@
 // }
 
 module.exports = {
-   host     : 'aa1aa8b889dc6x2.cnc53j0i9ssi.us-west-2.rds.amazonaws.com:3306',
+   host     : 'aa1aa8b889dc6x2.cnc53j0i9ssi.us-west-2.rds.amazonaws.com',
    user     : 'cobberDbAdmin',
    password : '123abc$%^',
    port     : '3306',
